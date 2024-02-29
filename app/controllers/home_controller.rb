88b8c0025c10970b1fa3c3d20086f654cd73d447
Logan Hand <loganhand96@gmail.com>
2024-02-29 12:21:42 -0700

    Added instance variables that read the JSON returned from the open weather API call. Built out some css styling classes for the current weather page. Added metrics to the home page such as cloud coverage, humidity, percipitation, current temp, high/low temp, and wind speed.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,29 @@ class HomeController < ApplicationController
     uri = URI(url)
     res = Net::HTTP.get_response(uri)
     @data = JSON.parse(res.body)
+
+    #checking if there is rain/snow data in the returned JSON
+    @rain = @data["rain"]
+    if @rain == nil
+        @rain = "None"
+    else
+      @rain = @rain["1h"]
+    end
+    @snow = @data["snow"]
+    if @snow == nil
+        @snow = "None"
+    else
+      @snow = @snow["1h"]
+    end
+
+    # preparing the data for display
+    @temp = @data["main"]["temp"].round
+    @temp_high = @data["main"]["temp_max"].round
+    @temp_low = @data["main"]["temp_min"].round
+    @wind_speed = @data["wind"]["speed"].round
+    @visibility = (@data["visibility"]/1000).round
+
+    @date = Date.today.strftime("%b") + " " + Date.today.strftime("%d")
   # puts res.body if res.is_a?(Net::HTTPSuccess)
   end
 end
